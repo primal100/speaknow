da5e826692e046c2ba3bd5eceb23e6165ba53de0
Paul Martin <greatestloginnameever@gmail.com>
2026-01-07 11:43:03 +0000

    Remove audio recording, add config options for storing tokens

--- speaknow_ai_realtime_text_to_speech/ai_services/base.py
+++ speaknow_ai_realtime_text_to_speech/ai_services/base.py
@@ -33,37 +33,60 @@ class BaseAIService(ABC):
 
     def __init__(self, user_config: dict[str, Any]):
         self.user_config = user_config
-        self.realtime_convo_csv = Path(TOKENS_DIR) / f"{self.prefix}_realtime_conversation_tokens.csv"
-        self.realtime_tokens_csv = Path(TOKENS_DIR) / f"{self.prefix}_realtime_transcribe_tokens.csv"
-        self.token_logger_realtime = TokenLogger(log_writer, self.realtime_pricing_cls(self.realtime_costs))
-        self.csv_writer_realtime = CSVWriter(self.realtime_convo_csv)
-        self.csv_writer_realtime_transcribe = CSVWriter(self.realtime_tokens_csv)
-        self.csv_token_logger_realtime = TokenLogger(self.csv_writer_realtime, self.realtime_pricing_cls(self.realtime_costs))
-        if self.transcription_pricing_cls:
-            self.token_logger_realtime_transcription = TokenLogger(log_writer, self.transcription_pricing_cls(
-                self.transcription_costs))
+        if self.user_config.get("save_token_data") and self.user_config["model"] in self.realtime_costs:
+            self.realtime_convo_csv = Path(TOKENS_DIR) / f"{self.prefix}_realtime_conversation_tokens.csv"
+            self.realtime_tokens_csv = Path(TOKENS_DIR) / f"{self.prefix}_realtime_transcribe_tokens.csv"
+            self.token_logger_realtime = TokenLogger(log_writer, self.realtime_pricing_cls(self.realtime_costs))
+            self.csv_writer_realtime = CSVWriter(self.realtime_convo_csv)
+            self.csv_writer_realtime_transcribe = CSVWriter(self.realtime_tokens_csv)
+            self.csv_token_logger_realtime = TokenLogger(self.csv_writer_realtime, self.realtime_pricing_cls(self.realtime_costs))
+        else:
+            self.realtime_convo_csv = None
+            self.realtime_tokens_csv = None
+            self.token_logger_realtime = None
+            self.csv_writer_realtime = None
+            self.csv_token_logger_realtime = None
+        if self.user_config.get("save_token_data") and self.transcription_pricing_cls and self.user_config['transcription_model'] in self.transcription_costs:
+            self.token_logger_realtime_transcription = TokenLogger(log_writer, self.transcription_pricing_cls(self.transcription_costs))
             self.csv_token_logger_realtime_transcription = TokenLogger(self.csv_writer_realtime_transcribe, self.transcription_pricing_cls(self.transcription_costs))
         else:
             self.token_logger_realtime_transcription = None
             self.csv_token_logger_realtime_transcription = None
+            self.token_logger_realtime_transcription = None
+            self.csv_writer_realtime_transcribe = None
+            self.csv_token_logger_realtime_transcription = None
 
     async def cleanup_resources(self):
-        await asyncio.gather(
-            asyncio.to_thread(self.token_logger_realtime.close),
-            asyncio.to_thread(self.csv_token_logger_realtime.close),
-        )
+        if self.token_logger_realtime:
+            await asyncio.gather(
+                asyncio.to_thread(self.token_logger_realtime.close),
+                asyncio.to_thread(self.csv_token_logger_realtime.close),
+            )
         if self.token_logger_realtime_transcription:
             await asyncio.gather(
                 asyncio.to_thread(self.token_logger_realtime_transcription.close),
                 asyncio.to_thread(self.csv_writer_realtime_transcribe.close)
         )
 
+    async def write_realtime_tokens(self, model: str, result: str, usage: Any) -> None:
+        self.token_logger_realtime.record(model, result, usage)
+        await asyncio.to_thread(self.csv_token_logger_realtime.record(model, result, usage))
 
-    @abstractmethod
-    def write_realtime_tokens(self, model: str, result: str, usage: Any) -> None: ...
+    async def write_realtime_transcribe_tokens(self, model: str, result: str, usage: Any) -> None:
+        self.token_logger_realtime_transcription.record(model, result, usage)
+        await asyncio.to_thread(self.csv_token_logger_realtime_transcription.record(model, result, usage))
 
-    @abstractmethod
-    def write_realtime_transcribe_tokens(self, model: str, result: str, usage: Any) -> None: ...
+    async def write_realtime_tokens_wrapper(self, model: str, result: str, usage: Any) -> None:
+        if not self.user_config['save_result']:
+            result = ""
+        if self.token_logger_realtime and self.csv_token_logger_realtime:
+            await self.write_realtime_tokens(model, result, usage)
+
+    async def write_realtime_transcribe_tokens_wrapper(self, model: str, result: str, usage: Any) -> None:
+        if not self.user_config['save_result']:
+            result = ""
+        if self.token_logger_realtime_transcription and self.csv_token_logger_realtime_transcription:
+            await self.write_realtime_transcribe_tokens(model, result, usage)
 
     @abstractmethod
     async def send_audio(self, data: ndarray, sent_audio: bool) -> bool: ...
@@ -76,7 +99,3 @@ class BaseAIService(ABC):
 
     @abstractmethod
     async def request_response_if_manual_mode(self) -> None: ...
-
-
-
-
